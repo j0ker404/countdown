f3c1ae4222387d74439087bdeb08557420de3107
joker <uanhermanus@gmail.com>
2020-12-07 01:39:54 +0200

    implement max input

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function numDigits(num) {
         num = Math.round(num / 10);
         digits++;
     }
-    console.log(digits);
+    // console.log(digits);
     return digits;   
 } 
 
@@ -12,26 +12,72 @@ class InputState {
 
     constructor(input) {
         this.input = input;
-        this.currVal = this.input.innerHTML;
+        // this.currVal = this.input.innerHTML;
+        this.currVal = 0;
         this.prevVal = 0;
+        this.numDigit = 0;
         this.input.addEventListener('input', this.updateInput);
     }
     
     updateInput(e) {
         // console.log(e.target.value);
-        this.currVal = e.target.value;
+        // this.currVal = e.target.value;
         
         // check if input is a number 
-        if (isNaN(this.currVal)) {
-            e.target.value = this.prevVal; 
-            this.currVal = e.target.value;
+        // if (isNaN(this.currVal)) {
+        //     e.target.value = this.prevVal; 
+        //     if (e.target.value > 999999) {
+        //         e.target.value = 999999;
+        //         console.log('hit');
+        //     }
+        //     this.currVal = e.target.value;
+        // } else {
+        //     console.log('yo mama');
+        //     this.prevVal = this.currVal;
+        //     if (this.prevVal > 999999) {
+        //         this.prevVal = 999999;
+        //         e.target.value = 999999
+        //     }
+        // }
+
+        this.currVal = e.target.value;
+        // if (isNaN(this.currVal)) {
+        //     e.target.value = this.prevVal; 
+        //     console.log('hit');
+        //     this.currVal = e.target.value;
+        // } else {
+        //     console.log('yo mama');
+        //     this.prevVal = this.currVal;
+        // }
+
+        if (!isNaN(this.currVal)) {
+            // console.log('yo mama');
+            if (this.currVal < 1000000) {
+                this.prevVal = this.currVal;
+            } else {
+                this.currVal = this.prevVal;
+                e.target.value = this.currVal;
+            }
         } else {
-            this.prevVal = this.currVal;
+            e.target.value = this.prevVal;
+            this.currVal = e.target.value;
         }
+
+        // if (this.numDigit < 6) {
+        //     this.currVal = e.target.value;
+        //     if (!isNaN(this.currVal)) {
+        //         console.log('yo mama');
+        //         this.prevVal = this.currVal;
+        //     }
+        // } else {
+        //     e.target.value = this.prevVal;
+        // }
         // from this point this.currVal is always a number
-        // console.log(this.currVal);
-        let numDigit = numDigits(this.currVal);
-        console.log(`numDigits:${numDigit}`); 
+        console.log(this.currVal);
+        this.numDigit = numDigits(this.currVal);
+        console.log(`numDigits:${this.numDigit}`); 
+        // 00h 00m 00s
+        
     }
 
 }
