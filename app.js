5fd6868380be4fa897df2d85edee8400a3358eff
joker <uanhermanus@gmail.com>
2020-12-07 03:16:04 +0200

    code refactor

--- app.js
+++ app.js
@@ -1,93 +1,12 @@
-function numDigits(num) {
-    let digits = 0;
-    while(num > 0 ) {
-        num = Math.round(num / 10);
-        digits++;
-    }
-    // console.log(digits);
-    return digits;   
-} 
 
-class InputState {
+let input = document.querySelector("input");
+// let inputState = new InputState(input);
 
-    constructor(input) {
-        this.input = input;
-        // this.currVal = this.input.innerHTML;
-        this.currVal = 0;
-        this.prevVal = 0;
-        this.numDigit = 0;
-        this.input.addEventListener('input', this.updateInput);
-    }
-    
-    updateInput(e) {
-        // console.log(e.target.value);
-        // this.currVal = e.target.value;
-        
-        // check if input is a number 
-        // if (isNaN(this.currVal)) {
-        //     e.target.value = this.prevVal; 
-        //     if (e.target.value > 999999) {
-        //         e.target.value = 999999;
-        //         console.log('hit');
-        //     }
-        //     this.currVal = e.target.value;
-        // } else {
-        //     console.log('yo mama');
-        //     this.prevVal = this.currVal;
-        //     if (this.prevVal > 999999) {
-        //         this.prevVal = 999999;
-        //         e.target.value = 999999
-        //     }
-        // }
-
-        this.currVal = e.target.value;
-        // if (isNaN(this.currVal)) {
-        //     e.target.value = this.prevVal; 
-        //     console.log('hit');
-        //     this.currVal = e.target.value;
-        // } else {
-        //     console.log('yo mama');
-        //     this.prevVal = this.currVal;
-        // }
-
-        if (!isNaN(this.currVal)) {
-            // console.log('yo mama');
-            if (this.currVal < 1000000) {
-                this.prevVal = this.currVal;
-            } else {
-                this.currVal = this.prevVal;
-                e.target.value = this.currVal;
-            }
-        } else {
-            e.target.value = this.prevVal;
-            this.currVal = e.target.value;
-        }
-
-        // if (this.numDigit < 6) {
-        //     this.currVal = e.target.value;
-        //     if (!isNaN(this.currVal)) {
-        //         console.log('yo mama');
-        //         this.prevVal = this.currVal;
-        //     }
-        // } else {
-        //     e.target.value = this.prevVal;
-        // }
-        // from this point this.currVal is always a number
-        console.log(this.currVal);
-        this.numDigit = numDigits(this.currVal);
-        console.log(`numDigits:${this.numDigit}`); 
-        // 00h 00m 00s
-        
-    }
-
-}
+let currVal = 0;
+let prevVal = 0;
+let numDigit = 0;
+let time = [];
+// let temp = 55;
+input.addEventListener("input", updateInput);
 
 
-let input = document.querySelector('input');
-let inputState = new InputState(input);
-
-// input.addEventListener('input',update);
-
-// function update(e) {
-//     console.log(e.target.value);
-// }
---
+++ functions.js
@@ -0,0 +1,58 @@
+// functions
+
+function updateInput(e) {
+    // console.log(`temp:${temp}`);
+    currVal = e.target.value;
+
+    if (!isNaN(currVal)) {
+        // console.log('yo mama');
+        if (currVal < 1000000) {
+            prevVal = currVal;
+        } else {
+            currVal = prevVal;
+            e.target.value = currVal;
+        }
+    } else {
+        // console.log("butrh");
+        // console.log(`prevVal:${prevVal}`);
+        e.target.value = prevVal;
+        currVal = e.target.value;
+    }
+
+    // from this point this.currVal is always a number
+    currVal = Math.floor(currVal);
+    e.target.value = currVal;
+    console.log(currVal);
+    numDigit = numDigits(currVal);
+    // console.log(`numDigits:${this.numDigit}`);
+    // 00h 00m 00s
+    time = updateTime(currVal);
+    console.log(time);
+}
+
+// return the number of digits
+function numDigits(num) {
+    let digits = 0;
+    while (num > 0) {
+    num = Math.round(num / 10);
+    digits++;
+    }
+    // console.log(digits);
+    return digits;
+}
+
+function updateTime(num) {
+    // console.log('test');
+    let temp = 0;
+    let n = num;
+    arr = [];
+
+    while (n > 0) {
+    temp = n % 100;
+    arr.push(temp);
+    n = Math.floor(n / 100);
+    }
+    // console.log(`arr:${arr}`);
+    // console.log(arr);
+    return arr;
+}
